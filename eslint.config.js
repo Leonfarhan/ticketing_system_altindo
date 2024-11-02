0586b58e8c940148efed7cf339003f2b800d6aa7
leonfarhan <leonfarhan404@gmail.com>
2024-11-02 09:54:44 +0700

    changes: update some line

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,91 @@
 import js from '@eslint/js';
-import globals from 'globals';
+import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import parser from '@typescript-eslint/parser';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
+const config = {
+    parser: parser.name,
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
+        project: ['./tsconfig.json'], // Menyesuaikan path ke tsconfig.json
+        tsconfigRootDir: __dirname,
     },
+    extends: [
+        js.configs.recommended.rules,
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    ].map(c => (typeof c === 'string' ? c : c.extends)), // Flattening the extends
+
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+        react,
+        'react-hooks': reactHooks,
+        'react-refresh': reactRefresh,
+        '@typescript-eslint': tseslint,
     },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+
+    overrides: [
+        {
+            files: ['*.ts', '*.tsx', '*.js', '*.jsx'],
+            extends: [
+                'plugin:react/jsx-runtime', //  menghilangkan react in jsx scope,
+            ],
+            rules: {
+                'react/prop-types': 'off', // Menonaktifkan prop-types karena menggunakan TypeScript
+                '@typescript-eslint/no-unused-vars': 'warn',
+                '@typescript-eslint/no-empty-interface': 'off',
+
+
+                // Menonaktifkan beberapa aturan unsafe-* untuk fleksibilitas:
+                '@typescript-eslint/no-unsafe-assignment': 'off',
+                '@typescript-eslint/no-floating-promises': 'off', //tambahan floating promises
+                '@typescript-eslint/no-unsafe-member-access': 'off',
+                '@typescript-eslint/no-unsafe-call': 'off',
+                '@typescript-eslint/no-unsafe-return': 'off',
+                '@typescript-eslint/restrict-template-expressions': 'off', //tambahan restrict-template-expressions
+                '@typescript-eslint/unbound-method': 'off', //tambahan unbound-method
+                '@typescript-eslint/no-misused-promises': 'off', //tambahan no-misused-promises
+                '@typescript-eslint/no-non-null-assertion': 'off', //tambahan no-non-null-assertion
+                '@typescript-eslint/ban-ts-comment': 'off', //tambahan ban-ts-comment
+                '@typescript-eslint/require-await': 'off', //tambahan require-await
+                'react/jsx-key': ['warn', { "checkFragmentShorthand": true }], // Memeriksa juga fragment shorthand
+
+                'no-console': 'warn',
+
+            },
+        },
+        {
+            files: ['backend/**/*.ts'],
+            rules: {
+            },
+        },
+    ],
+
+    settings: {
+        react: {
+            version: 'detect',
+        },
     },
-  }
-);
+
+    // Konfigurasi global (jika diperlukan)
+    env: {
+        browser: true,
+        node: true, //tambahan node
+    },
+
+};
+
+export default config;
